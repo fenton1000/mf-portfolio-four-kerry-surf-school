edb56ee4ff8014c18832e639e31c593591aa2577
fenton1000 <fenton1000@yahoo.co.uk>
2023-03-04 16:53:21 +0000

    Add the surfing lesson info

--- static/js/script.js
+++ static/js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     function changeHomeView() {
         if (window.matchMedia("(max-width: 650px)").matches) {
             $( "#accordion" ).removeClass( "make-div-disappear" ).addClass( "make-div-appear" );
-            $( "#cards" ).removeClass( "make-div-appear" ).addClass( "make-div-disappear" );
+            $( "#cards, #lessoninfo" ).removeClass( "make-div-appear" ).addClass( "make-div-disappear" );
         } else {
-            $( "#cards" ).removeClass( "make-div-disappear" ).addClass( "make-div-appear" );
+            $( "#cards, #lessoninfo" ).removeClass( "make-div-disappear" ).addClass( "make-div-appear" );
             $( "#accordion" ).removeClass( "make-div-appear" ).addClass( "make-div-disappear" );
         }
     }
