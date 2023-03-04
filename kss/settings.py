a75198a1f7410651b0085a8119481876e9ffaa55
fenton1000 <fenton1000@yahoo.co.uk>
2023-03-04 20:05:47 +0000

    Set debug value from env file

--- kss/settings.py
+++ kss/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_STATUS')
 
 ALLOWED_HOSTS = ['mf-pp4-kerry-surf-school.herokuapp.com', 'localhost']
 
