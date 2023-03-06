0a2e103d9dd25bc14cf0e3651290af42ca5f9e30
fenton1000 <fenton1000@yahoo.co.uk>
2023-03-06 22:00:54 +0000

    Add the customer model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True,)
+    email = models.EmailField()
+    phone_num = models.CharField(max_length=20)
