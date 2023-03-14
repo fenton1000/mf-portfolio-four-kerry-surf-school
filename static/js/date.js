4e79a4b2edffcdae798ac35b60990ea81f1c1d13
fenton1000 <fenton1000@yahoo.co.uk>
2023-03-14 11:08:15 +0000

    Prevent booking dates in the past

---
+++ static/js/date.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    function setMinDate() {
+        let minDate = new Date();
+        let formattedMinDate = new Date(
+            minDate.getTime() - (minDate.getTimezoneOffset() * 60000))
+            .toISOString()
+            .split("T")[0];
+            $('#id_lesson_date').attr('min', formattedMinDate);
+    }
+
+    setMinDate();
+});
