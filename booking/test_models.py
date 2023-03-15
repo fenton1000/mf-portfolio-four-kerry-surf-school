d90762c2c94ec2c28585e51dc3a31b45a6e7a79b
fenton1000 <fenton1000@yahoo.co.uk>
2023-03-15 22:49:03 +0000

    Add auto tests for model label string functions

--- booking/test_models.py
+++ booking/test_models.py
@@ -35,3 +35,29 @@ class TestModels(TestCase):
         self.assertEqual(len(customers), 0)
         bookings = Booking.objects.filter(customer=user)
         self.assertEqual(len(bookings), 0)
+
+    def test_customer_string_method_returns_username(self):
+        user = User.objects.create(
+            username='johns', password='3450123DGee%%')
+        customer = Customer.objects.create(
+            user=user,
+            email='john@email.ie',
+            phone_num='555 123 4567'
+        )
+        self.assertEqual(str(customer), 'johns')
+
+    def test_booking_string_method_returns_lesson_date(self):
+        user = User.objects.create(
+            username='johns', password='3450123DGee%%')
+        booking = Booking.objects.create(
+            customer=user,
+            first_name='John',
+            last_name='Smith',
+            date_of_birth='1980-04-10',
+            height='1.8',
+            weight='65',
+            ability_level='Beginner',
+            lesson_date='2023-10-10',
+            lesson_time='09:00',
+        )
+        self.assertEqual(str(booking), '2023-10-10')
