d83d5547d898b40d5502797c08c8e06b3b5ac56a
fenton1000 <fenton1000@yahoo.co.uk>
2023-03-21 15:57:26 +0000

    Alter the booking and customer models

--- booking/models.py
+++ booking/models.py
@@ -5,8 +5,15 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, primary_key=True,)
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
     phone_num = models.CharField(max_length=20, null=False, blank=False)
+    date_of_birth = models.DateField(null=False, blank=False)
+    height = models.DecimalField(
+        max_digits=3, decimal_places=2, null=False, blank=False)
+    weight = models.DecimalField(
+        max_digits=3, decimal_places=1, null=False, blank=False)
 
     def __str__(self):
         return self.user.username
@@ -15,16 +22,11 @@ class Customer(models.Model):
 class Booking(models.Model):
     customer = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='bookings')
-    first_name = models.CharField(max_length=100, null=False, blank=False)
-    last_name = models.CharField(max_length=100, null=False, blank=False)
-    date_of_birth = models.DateField(null=False, blank=False)
-    height = models.DecimalField(
-        max_digits=3, decimal_places=2, null=False, blank=False)
-    weight = models.DecimalField(
-        max_digits=3, decimal_places=1, null=False, blank=False)
     ability_level = models.CharField(max_length=50, null=False, blank=False)
     lesson_date = models.DateField(null=False, blank=False)
     lesson_time = models.TimeField(null=False, blank=False)
+    customer_requests = models.TextField()
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.lesson_date)
--- booking/test_views.py
+++ booking/test_views.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.messages import get_messages
 from django.shortcuts import get_object_or_404
 from .models import Booking, Customer
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 class TestViews(TestCase):
