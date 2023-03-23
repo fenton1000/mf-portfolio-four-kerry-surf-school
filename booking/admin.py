3c18a54d8b8befacba541057e9c0f4f7bcef3bc8
fenton1000 <fenton1000@yahoo.co.uk>
2023-03-23 22:02:02 +0000

    Add booking approval action to admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Customer, Booking
 
-# Register your models here.
 admin.site.register(Customer)
-admin.site.register(Booking)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('lesson_date', 'lesson_time', 'ability_level', 'approved')
+    list_filter = ('lesson_date', 'approved')
+    actions = ['approve_selected_bookings', 'disapprove_selected_bookings']
+
+    def approve_selected_bookings(self, request, queryset):
+        queryset.update(approved=True)
+
+    def disapprove_selected_bookings(self, request, queryset):
+        queryset.update(approved=False)
